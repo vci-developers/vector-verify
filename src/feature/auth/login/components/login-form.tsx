425dd0bd9579da0d784cbf4185a91f1e4d8b1ab4
Aryaman Shodhan <ashodha1@jhu.edu>
2025-08-21 02:51:56 -0400

    rename files

--- src/feature/auth/login/components/login-form.tsx
+++ src/feature/auth/login/components/login-form.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/inpu";
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/butto";
+import { Checkbox } from "@/components/ui/checkbo";
 import { Fragment } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
