6dd24335ca83c9fd6a5e1f62edb13fd0a29bffae
Aryaman Shodhan <ashodha1@jhu.edu>
2025-08-28 13:42:13 -0400

    add prettier config file

---
+++ prettier.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: "all",
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: "avoid",
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  useTabs: false,
+};
