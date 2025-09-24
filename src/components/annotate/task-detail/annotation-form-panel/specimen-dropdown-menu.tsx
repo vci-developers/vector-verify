ccade49d5852a5be04224e4098ee92d0a03ad90d
vinnychi123 <irishspring243@gmail.com>
2025-09-24 18:29:32 -0400

    specimen dropdown menu
    created component for dropdown menu, still needs to be able to provid options when implemented, still need to make clear selection

---
+++ src/components/annotate/task-detail/annotation-form-panel/specimen-dropdown-menu.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { toDomId } from "@/lib/shared/utils/dom";
+import { ChevronDown } from "lucide-react";
+import { de } from "zod/v4/locales";
+import { on } from "events";
+
+interface SpecimenDropdownMenuProps {
+    label: string;
+    specimenIds: readonly string[];
+    selectedSpecimenId: string | undefined;
+    onSpecimenSelect: (specimenId?: string) => void;
+    enabled?: boolean;
+}
+
+export default function SpecimenDropdownMenu({
+    label,
+    specimenIds,
+    selectedSpecimenId,
+    onSpecimenSelect,
+    enabled = true,
+}: SpecimenDropdownMenuProps) {
+    function specimenIdSelectHandler(specimenId?: string) {
+        onSpecimenSelect(specimenId);
+    }
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    id={toDomId(label)}
+                    variant="outline"
+                    className={cn(
+                        "w-full justify-between",
+                        !enabled && "pointer-events-none opacity-50"
+                    )}
+                    disabled={!enabled}
+                >
+                    {enabled ? (
+                        selectedSpecimenId ?(
+                            <span> {selectedSpecimenId} </span>
+                        ) : (
+                            <span className="text-muted-foreground">Select {label}</span>
+                        )
+                    ) : (
+                        <span className="text-muted-foreground"> Not Applicable </span>
+                    )}
+                    <ChevronDown className="ml-2 h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+
+        </DropdownMenu>
+
+    )
+}
+
+
+
