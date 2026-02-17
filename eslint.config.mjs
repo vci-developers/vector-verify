105755a9f6a48e96d484514a65019a15580f054e
Aryaman Shodhan <ashodha1@jhu.edu>
2026-02-17 00:41:47 -0500

    VCV-97: Add strict configurations to ensure good practices

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,98 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import boundaries from 'eslint-plugin-boundaries';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts',
+        ],
+    },
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        plugins: { boundaries },
+        settings: {
+            'boundaries/include': ['src/**/*'],
+            'boundaries/elements': [
+                {
+                    mode: 'full',
+                    type: 'shared',
+                    pattern: [
+                        'src/components/**/*',
+                        'src/data/**/*',
+                        'src/hooks/**/*',
+                        'src/lib/**/*',
+                        'src/server/**/*',
+                    ],
+                },
+                {
+                    mode: 'full',
+                    type: 'feature',
+                    capture: ['featureName'],
+                    pattern: ['src/features/*/**/*'],
+                },
+                {
+                    mode: 'full',
+                    type: 'app',
+                    capture: ['_', 'fileName'],
+                    pattern: ['src/app/**/*'],
+                },
+                {
+                    mode: 'full',
+                    type: 'neverImport',
+                    pattern: ['src/*.*', 'src/tasks/**/*'],
+                },
+            ],
+        },
+
+        rules: {
+            'boundaries/no-unknown': 'error',
+            'boundaries/no-unknown-files': 'error',
+
+            'boundaries/element-types': [
+                'error',
+                {
+                    default: 'disallow',
+                    rules: [
+                        {
+                            from: ['shared'],
+                            allow: ['shared'],
+                        },
+                        {
+                            from: ['feature'],
+                            allow: [
+                                'shared',
+                                [
+                                    'feature',
+                                    { featureName: '${from.featureName}' },
+                                ],
+                            ],
+                        },
+                        {
+                            from: ['app', 'neverImport'],
+                            allow: ['shared', 'feature'],
+                        },
+                        {
+                            from: ['app'],
+                            allow: [['app', { fileName: '*.css' }]],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+];
 
 export default eslintConfig;
