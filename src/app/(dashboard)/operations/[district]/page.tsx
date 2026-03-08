65e1553ab37f513d6af355471fc2d77195d150cc
vinnychi123 <irishspring243@gmail.com>
2026-03-08 15:50:21 -0400

    Update page.tsx

--- src/app/(dashboard)/operations/[district]/page.tsx
+++ src/app/(dashboard)/operations/[district]/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useParams } from 'next/navigation';
 import { useGetUserPermissions } from '@/api/user/hooks/use-get-user-permissions';
 import type { UserPermissions } from '@/api/user/validation/user-permissions-schema';
