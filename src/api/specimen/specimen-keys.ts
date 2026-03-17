4e0edaec21fb7500e71580cbbce0b6eddd79df0a
Cole Manel <csmanel@gmail.com>
2026-03-17 11:55:42 -0400

    VCV-135: implement BFF for /specimens/all

--- src/api/specimen/specimen-keys.ts
+++ src/api/specimen/specimen-keys.ts
@@ -1,3 +1,4 @@
+import type { GetAllSpecimensQueryParams } from '@/api/specimen/validation/get-all-specimens-schema';
 import type { GetSpecimensCountQueryParams } from '@/api/specimen/validation/get-specimens-count-schema';
 import type { GetSpecimensQueryParams } from '@/api/specimen/validation/get-specimens-schema';
 
@@ -8,4 +9,6 @@ export const specimenKeys = {
         ['specimens', queryParams] as const,
     specimensCount: (queryParams?: GetSpecimensCountQueryParams) =>
         ['specimens', 'count', queryParams] as const,
+    allSpecimens: (queryParams?: GetAllSpecimensQueryParams) =>
+        ['specimens', 'all', queryParams] as const,
 };
