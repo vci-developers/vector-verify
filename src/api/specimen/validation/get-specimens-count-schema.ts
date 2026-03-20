2fc31154a920498721031b852082fb4df4e445e9
Cole Manel <csmanel@gmail.com>
2026-03-20 11:07:56 -0400

    VCV-135: Create API endpoints on the BFF (#76)
    * VCV-135: add GET/specimens/count schema
    
    * VCV-135: add get-specimens-count
    
    * VCV-135: Implement route for specimen count, hook for use-get-specimens-count, and add specimens count to specimens key
    
    * VCV-135: implement BFF for /specimens/all
    
    * VCV-135: update get-specimens-count to use sessionTypeSchema and siteSchema, use-get-all-specimens to require GetAllSpecimensQueryParams, get-all-specimens-schema to require district and use sessionTypeSchema
    
    * VCV-135: remove unnecessary exports from specimensCountSchema and countDataSchema

---
+++ src/api/specimen/validation/get-specimens-count-schema.ts
@@ -0,0 +1,48 @@
+import { sessionTypeSchema } from '@/api/session/validation/session-schema';
+import { siteSchema } from '@/api/site/validation/site-schema';
+import { z } from 'zod';
+
+export const getSpecimensCountQueryParamsSchema = z.object({
+    sessionId: z.coerce.number().optional(),
+    siteId: z.coerce.number().optional(),
+    programId: z.coerce.number().optional(),
+    district: z.string().optional(),
+    specimenId: z.string().optional(),
+    shouldProcessFurther: z.coerce.boolean().optional(),
+    hasImages: z.coerce.boolean().optional(),
+    species: z.string().optional(),
+    sex: z.string().optional(),
+    abdomenStatus: z.string().optional(),
+    sessionType: sessionTypeSchema.optional(),
+    startDate: z.string().optional(),
+    endDate: z.string().optional(),
+});
+
+const specimensCountSchema = z.object({
+    species: z.string(),
+    sex: z.string(),
+    abdomenStatus: z.string(),
+    count: z.number(),
+    columnName: z.string(),
+});
+
+const specimensCountDataSchema = z.object({
+    siteId: z.number().optional(),
+    siteInfo: siteSchema.optional(),
+    counts: z.array(specimensCountSchema),
+    totalSpecimens: z.number(),
+});
+
+export const getSpecimensCountResponseSchema = z.object({
+    message: z.string(),
+    columns: z.array(z.string()),
+    data: z.array(specimensCountDataSchema),
+});
+
+export type GetSpecimensCountQueryParams = z.infer<
+    typeof getSpecimensCountQueryParamsSchema
+>;
+export type GetSpecimensCountResponseBody = z.infer<
+    typeof getSpecimensCountResponseSchema
+>;
+export type GetSpecimensCountSuccessPayload = GetSpecimensCountResponseBody;
