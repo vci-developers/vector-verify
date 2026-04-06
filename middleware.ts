9da78bb0e1618e2aeff69660dd5fd0534333452c
Cole Manel <csmanel@gmail.com>
2026-04-06 17:04:46 -0400

    VCV-160: prettier formatting

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,5 @@ import { routing } from './src/i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-    matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
