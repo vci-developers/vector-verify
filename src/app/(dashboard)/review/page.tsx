47e5c9b6d3b4d579e20361d58cbe4210db7d5e86
Aryaman Shodhan <ashodha1@jhu.edu>
2026-04-22 00:45:45 -0400

    VCV-185: Create tab for sites list in review section

--- src/app/(dashboard)/review/page.tsx
+++ src/app/(dashboard)/review/page.tsx
@@ -1,7 +1,6 @@
 import { getUserPermissions } from '@/api/user/get-user-permissions';
 import type { GetUserPermissionsResponseBody } from '@/api/user/validation/get-user-permissions-schema';
 import { userKeys } from '@/api/user/user-keys';
-import ReviewSitesListPageClient from '@/features/review/components/sites-list/page-client/review-sites-list-page-client';
 import { withAuthSession } from '@/lib/auth-session/with-auth-session';
 import {
     dehydrate,
@@ -9,6 +8,7 @@ import {
     QueryClient,
 } from '@tanstack/react-query';
 import { redirect } from 'next/navigation';
+import ReviewPageClient from '@/features/review/components/page-client/review-page-client';
 
 export default async function ReviewSiteListPage() {
     const queryClient = new QueryClient();
@@ -37,7 +37,7 @@ export default async function ReviewSiteListPage() {
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <ReviewSitesListPageClient />
+            <ReviewPageClient />
         </HydrationBoundary>
     );
 }
