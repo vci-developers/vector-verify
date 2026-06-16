7a6f829839051d8f44616037fa6db1fef27a6ba3
Cole Manel <csmanel@gmail.com>
2026-06-16 16:16:07 -0400

    Update page.tsx

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from 'next/navigation';
 // import DashboardPageClient from '@/features/dashboard/components/dashboard-page-client';
 
 export default function DashboardPage() {
-    // Temporarily redirecting while the dashboard is under construction.
     redirect('/operations');
     // return <DashboardPageClient />;
 }
