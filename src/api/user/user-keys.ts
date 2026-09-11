0a6a98ee2ab045dc15a41f07ce0a2cc13af03992
Cole Manel <csmanel@gmail.com>
2026-09-11 13:55:25 -0400

    VCV-303: Update BFF

--- src/api/user/user-keys.ts
+++ src/api/user/user-keys.ts
@@ -1,6 +1,5 @@
 import type { GetAllUserActiveMetricsQueryParams } from '@/api/user/validation/get-all-user-active-metrics-schema';
 import type { GetUserAuthEventsQueryParams } from '@/api/user/validation/get-user-auth-events-schema';
-import type { GetUsersQueryParams } from '@/api/user/validation/get-users-schema';
 
 export const userKeys = {
     permissions: () => ['user', 'permissions'] as const,
@@ -9,6 +8,4 @@ export const userKeys = {
         ['user', 'active-metrics', 'all', queryParams] as const,
     authEvents: (queryParams: GetUserAuthEventsQueryParams) =>
         ['user', 'auth-events', queryParams] as const,
-    users: (queryParams: GetUsersQueryParams) =>
-        ['user', 'list', queryParams] as const,
 };
